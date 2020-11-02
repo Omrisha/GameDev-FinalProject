d6aa2bfb47ffdaac6beac60ca506fd319e55b292
Omri Shapira <omris11@mail.afeka.ac.il>
2020-11-02 14:23:17 +0200

    Adding Doors + DoorMotion script + canvas and text for showing number of people

--- GameDev-FinalProject/Assets/Scripts/TrainMotion.cs
+++ GameDev-FinalProject/Assets/Scripts/TrainMotion.cs
@@ -5,17 +5,20 @@ using UnityEngine.AI;
 
 public class TrainMotion : MonoBehaviour
 {
+    public Animator animator;
     public GameObject target;
     NavMeshAgent theAgent;
     // Start is called before the first frame update
     void Start()
     {
+        //animator = GetComponent<Animator>();
         theAgent = GetComponent<NavMeshAgent>();
-        theAgent.SetDestination(target.transform.position);
+        theAgent.SetDestination(target.transform.localPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //animator.Play("Start");
     }
 }
